431dbd1607cf045a2096183a283a8fec7c229f35
awollac <awollac@wireless-10-105-227-58.umd.edu>
2017-10-09 23:15:08 -0400

    stuff

--- CirclePath.m
+++ CirclePath.m
@@ -1,6 +1,7 @@
 node_list = [Node(0,0), Node(0,1), Node(1,0),Node(1,1)];
 list_length = numel(node_list);
 
+
 average_x = 0;
 average_y = 0;
 for i = 1:list_length
@@ -10,10 +11,51 @@ end
 average_x = average_x/list_length;
 average_y = average_y/list_length;
 
-edge_node = [];
+edge_node = [0, 0];
 for i = 1:list_length
     base_vector = [average_x - node_list(i).xcoord, average_y - node_list(i).ycoord];
-    node_list(i).setBaseVector(base_vector);
-    if(~isEmpty(edge_node) && norm(base_vector) > edge_node(2))
-        edge_node(1) = node_
+    node_list(i).basevector = base_vector;
+    node_list(i).basemag = norm(base_vector);
+    if(node_list(i).basemag > edge_node(2))
+        edge_node(1) = i;
+        edge_node(2) = node_list(i).basemag;
+    end
 end
+
+first = true;
+current = edge_node(1);
+bigest_node = 0;
+bigest_angle = 0;
+last = 0;
+while(first || ~(current == edge_node(1)))
+    for i = 1:list_length
+        if(i ~= current && i ~= last)
+            angle = node_list(current).angleToVector(node_list(i));
+            if(angle > bigest_angle)
+                bigest_angle = angle;
+                bigest_node = i;
+            end
+        end
+    end
+
+    node_list(current).set('connected',node_list(bigest_node));
+    last = current;
+    current = bigest_node;
+    bigest_angle = 0;
+    first = false;
+end
+node_list(1)
+node_list(2)
+node_list(3)
+node_list(4)
+
+path = node_list(edge_node(1)).path(node_list(edge_node(1)));
+path_length = numel(path);
+path_x = [];
+path_y = [];
+for i = 1:path_length
+    path_x = [path_x , path(i).xcoord]
+    path_y = [path_y, path(i).ycoord]
+end
+
+plot(path_x,path_y);
--- Node.m
+++ Node.m
@@ -1,4 +1,4 @@
-classdef Node
+classdef Node < matlab.mixin.SetGet
     %NODE Summary of this class goes here
     %   Detailed explanation goes here
     
@@ -9,6 +9,7 @@ classdef Node
         xcoord
         ycoord
         basevector
+        basemag
     end
     
     methods
@@ -22,18 +23,33 @@ classdef Node
                 nodeObj.setConnected(node);
             end
         end
-        function setConnected(nodeObj, node)
+        function set.connected(nodeObj, node)
             nodeObj.distance = ((nodeObj.xcoord-node.xcoord)^2+ (nodeObj.ycoord-node.ycoord)^2)^(1/2);
             
             dot_product = ((nodeObj.xcoord-node.xcoord)*nodeObj.basevector(1) + (nodeObj.ycoord-node.ycoord)*nodeObj.basevector(2));
             magnitude_basevector = (nodeObj.basevector(1)^2 + nodeObj.basevector(2)^2)^(1/2);
             magnitude_vector = ((nodeObj.xcoord-node.xcoord)^2 + (nodeObj.ycoord-node.ycoord)^2)^(1/2);
             nodeObj.angle = (dot_product/(magnitude_basevector*magnitude_vector));
-            
             nodeObj.connected = node;
         end
-        function setBaseVector(nodeObj,basevector)
-            nodeObj.basevector = basevector;
+        function angle = angleToVector(nodeObj, node)
+            dot_product = abs(dot(nodeObj.basevector, node.basevector));
+            angle = acos(dot_product/(nodeObj.basemag*node.basemag))*180/pi;
+        end
+        function flag = equals(nodeObj, node)
+            if(nodeObj.xcoord == node.xcoord && nodeObj.ycoord == node.ycoord)
+                flag = true;
+            else
+                flag = false;
+            end
+        end
+        function [r] = path(nodeObj, end)
+            
+            if(isempty(nodeObj.connected)|| nodeObj.equals(end))
+                r = nodeObj;
+            else
+                r = [nodeObj, nodeObj.connected.path(end)];
+            end
         end
     end
     
