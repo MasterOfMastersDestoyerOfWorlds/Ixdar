a9e7b3eacc852a889378e03712f757981a5f5eea
awollac <awollac@wireless-10-105-227-58.umd.edu>
2017-10-16 16:24:17 -0400

    OBAMA HILLARY AND ROCKEFELLER TOOK A COLLECTIVE(.java)  DUMP IN THE POOL

--- RecalculatePerimeter.m
+++ RecalculatePerimeter.m
@@ -10,20 +10,38 @@ function [perimeter, closest_node] = RecalculatePerimeter(node_list, perimeter)
     for i = 1:list_length
         for j = 1:num_segments
             segment = [perimeter(j).vector,0];
-            %segment = [perimeter(j).xcoord - perimeter(j).connected.xcoord, perimeter(j).ycoord - perimeter(j).connected.ycoord,0];
             point_vector = [perimeter(j).xcoord - node_list(i).xcoord, perimeter(j).ycoord - node_list(i).ycoord,0];
             mag_segment = norm(segment);
             projection_multiplier = (dot(segment, point_vector)/(mag_segment)^2);
             
             distance_to_segment = Inf;
             flag = false;
-            if(projection_multiplier < -1)
+            if(projection_multiplier <= 0)
                 distance_to_segment = norm([perimeter(j).xcoord - node_list(i).xcoord, perimeter(j).ycoord - node_list(i).ycoord]);
-            elseif(projection_multiplier > 1)
+            elseif(projection_multiplier >= 1)
                 flag = true;
                 distance_to_segment = norm([perimeter(j).connected.xcoord - node_list(i).xcoord, perimeter(j).connected.ycoord - node_list(i).ycoord]);
             else
                 distance_to_segment = norm(cross(segment,point_vector))/norm(segment);
+            end
+            if(distance_to_segment == closest_distance)
+                first = [closest_node.xcoord - segment_location.xcoord, closest_node.ycoord - segment_location.ycoord];
+                dot_product = dot(first, segment_location.basevector);
+                first_angle = acos(dot_product/(norm(first)*norm(segment_location.basevector)))*180/pi;
+                
+                second = [node_list(i).xcoord - perimeter(j).xcoord, node_list(i).ycoord - perimeter(j).ycoord];
+                dot_product = dot(first, perimeter(j).basevector);
+                second_angle = acos(dot_product/(norm(second)*norm(perimeter(j).basevector)))*180/pi;
+
+                if(first_angle > second_angle)
+                    closest_node = node_list(i);
+                    closest_distance = distance_to_segment;
+                    if(~flag)
+                        segment_location = perimeter(j);
+                    else
+                        segment_location = perimeter(j).connected;
+                    end
+                end
             end
             if(distance_to_segment<closest_distance)
                 closest_node = node_list(i);
