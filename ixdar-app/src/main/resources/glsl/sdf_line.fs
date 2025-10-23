0cff1d85b97e5ead419c174c436ce255bd4226ee
Andrew Wollack <andrew.wollack@gmail.com>
2025-10-23 04:34:43 -0400

    shader update

--- ixdar-app/src/main/resources/glsl/sdf_line.fs
+++ ixdar-app/src/main/resources/glsl/sdf_line.fs
@@ -17,10 +17,9 @@ out vec4 fragColor;
 
 void main() {
 
-    float sigDist = -1.;
     float t = max(0., min(1., dot(scaledTextureCoord - pointA, pointB - pointA) * inverseLineLengthSq));
     vec2 projection = pointA + t * (pointB - pointA);
-    sigDist = distance(scaledTextureCoord, projection);
+    float sigDist = distance(scaledTextureCoord, projection);
     float opacity = smoothstep(edgeDist, edgeDist - edgeSharpness, sigDist);
     fragColor = vec4(mix(vertexColor.rgb, linearGradientColor.rgb, textureCoord.x), opacity * linearGradientColor.a);
 
